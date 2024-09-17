fb1f1230fc81236f86d9e579819cc7ded6f8db81
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-09-17 10:01:00 +0700

    <refactor> [refactor database and code]

---
+++ app/Controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends CoreController
+{
+    public function index()
+    {
+        $message = "Welcome to the Library API v2. Use the endpoints provided to interact with the library's resources. enjoyy :)";
+
+        return $this->respondWithSuccess($message, null, 200);
+    }
+}
