1985ac8e4dcc402e5a5a7a4ed916e01c68573eae
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-09-17 10:16:10 +0700

    check ci/cd

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends CoreController
 {
     public function index()
     {
-        $message = "Welcome to the Library API v2. Use the endpoints provided to interact with the library's resources. enjoyy :)";
+        $message = "Welcome to the Library API v2. Use the endpoints provided to interact with the library's resources. enjoyy broo :)";
 
         return $this->respondWithSuccess($message, null, 200);
     }
