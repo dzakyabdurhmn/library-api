121614b48de2311a62072a06c8bfaa4499afb19b
Dzaky Abdurahman <100127440+dzakyabdurhmn@users.noreply.github.com>
2024-09-18 10:49:13 +0700

    initial for v2 api

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,12 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends CoreController
+class Home extends BaseController
 {
-    public function index()
+    public function index(): string
     {
-        $message = "Welcome to the Library API v2. Use the endpoints provided to interact with the library's resources. enjoyy broo :)";
-
-        return $this->respondWithSuccess($message, null, 200);
+        return view('welcome_message');
     }
 }
